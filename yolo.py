c22bd59ba89d7bf1336417d9d0fc1f1508464189
muthoninduhiu <muthoninduhiu.mn@gmail.com>
2023-03-20 16:19:41 +0300

    create a report for the detected items

--- yolo.py
+++ yolo.py
@@ -24,6 +24,28 @@ img = cv2.resize(img, (640, 640))
 results = model(img)
 
 print("Results:{}".format(results))
+
+# Define an empty list to store the object detection results
+detections = []
+
+# Iterate over the results object and extract the relevant information
+for i in range(len(results.xyxy[0])):
+    bbox = results.xyxy[0][i]
+    label = results.names[int(bbox[5])]
+    score = bbox[4]
+    x1, y1, x2, y2 = map(int, bbox[:4])
+
+    # Append the object detection results to the detections list
+    detections.append({
+        'label': label,
+        'score': score,
+        'bbox': (x1, y1, x2, y2)
+    })
+
+# Print the object detection results
+for detection in detections:
+    print(f"Label: {detection['label']}, Score: {detection['score']:.2f}")
+
 # Iterate over the results object and display the image with bounding boxes around each detected object
 for i in range(len(results.xyxy[0])):
     bbox = results.xyxy[0][i]
@@ -36,5 +58,3 @@ for i in range(len(results.xyxy[0])):
 cv2.imshow('Image', img)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
-
-
