d4eea3fbdd62596423083865f269bb74df6e8b67
muthoninduhiu <muthoninduhiu.mn@gmail.com>
2023-03-21 15:00:18 +0300

    add score threshhold of 0.4

--- visualization.py
+++ visualization.py
@@ -8,15 +8,26 @@ for i in range(len(results.xyxy[0])):
     bbox = results.xyxy[0][i]
     label = results.names[int(bbox[5])]
     score = bbox[4]
-    x1, y1, x2, y2 = map(int, bbox[:4])
-    cv2.rectangle(img, (x1, y1), (x2, y2), (200, 21, 255), 3)
-    cv2.putText(img, f"{label} {score:.2f}", (x1, y1 - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.6, (255, 0, 66), 2)
 
+    # Only draw labels with a score of 0.5 and above
+    if score >= 0.4:
+        x1, y1, x2, y2 = map(int, bbox[:4])
+        cv2.rectangle(img, (x1, y1), (x2, y2), (200, 21, 255), 3)
+        cv2.putText(img, f"{label} {score:.2f}", (x1, y1 - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.6, (0, 0, 255), 2)
+
+        # Display the labels separately for each object
+        for detection in detections:
+            if detection['bbox'] == list(bbox):
+                labels = detection['label'].split(',')
+                y_offset = 20
+                for label in labels:
+                    cv2.putText(img, label.strip(), (x1, y1 - y_offset), cv2.FONT_HERSHEY_SIMPLEX, 0.6, (255, 0, 66), 2)
+                    y_offset += 20
 
 # Print the object detection results
 for detection in detections:
-    print(f"Label: {detection['label']}, Score: {detection['score']:.2f}")
+    print(f"Label: {detection['label']}, Score: {detection['score']:.2f}, Boxes: {detection['bbox']}")
+
 cv2.imshow('Image', img)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
-
