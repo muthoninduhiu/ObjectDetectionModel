ecb7249cb4ea1357d9df17e37172d8360670d3c9
muthoninduhiu <muthoninduhiu.mn@gmail.com>
2023-03-22 13:38:23 +0300

    create main file

---
+++ main.py
@@ -0,0 +1,11 @@
+import cv2
+from prediction_results import extract_predictions
+from visualization import visualize
+from yolo import load_model, resize_image, detect_objects, filter_results, count_objects
+
+
+def main():
+   
+
+if __name__ == '__main__':
+    main()
